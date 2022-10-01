b4ce170fdf30b8ced8a4d25bf6ce4ef08103e4c2
youjunl <Visungky@gmail.com>
2022-10-01 16:01:13 +0200

    update project to MSVC

--- Experiments/mini_dataset.m
+++ Experiments/mini_dataset.m
@@ -2,13 +2,15 @@
 ref = [0.01, 0.58, 0.84, 0.94, 0.97, 0.99, 0.997, 1, 1, 1, 1, 1, 1];
 dat = [0.00, 0.00, 0.0526316, 0.157895, 0.263158, 0.315789, 0.631579, 0.684211, 0.789474, ...
     0.842105, 0.947368, 0.947368, 0.947368];
+dat_10 = [0, 0.1, 0.2, 0.4, 0.4, 0.4, 0.7, 0.8, 0.9, 1, 1, 1, 1];
 round = 2:2:26;
 % 
 figure(1)
 hold on
 plot(round, dat, '-*');
+plot(round, dat_10, '-s');
 plot(round, ref, '-^');
-legend('Reporduced - 510 Reads 20 Centers','Paper - 5.3B reads, 10 Centers')
+legend('Reporduced - 510 Reads 20 Centers', 'Reporduced - 297 Reads 10 Centers','Paper - 5.3B reads, 10 Centers')
 title('Distributed Convergence (\gamma = 0.9)')
 xlabel('Communication Rounds')
 ylabel('Accuracy')
@@ -31,4 +33,8 @@ plot(ref_axis, ref_alg, '-s');
 plot(ref_axis, ref_scd8, '-^');
 plot(ref_axis, ref_scd6, '-o');
 
-legend()
+legend('Reproduced', 'Paper, proposed alg', 'Paper, Starcode(d=8)', 'Paper, Starcode(d=6)');
+grid on
+title('Accuracy Comparison')
+xlabel('\gamma')
+ylabel('Accuracy')
